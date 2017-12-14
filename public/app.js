92e82d187ed18cc8d2ea5d278447b96529c1e457
Yan Ren <yannyren@gmail.com>
2017-12-14 09:12:48 +0000

    mvp

---
+++ public/app.js
@@ -0,0 +1,46 @@
+const app = function(){
+  const url = 'https://api.punkapi.com/v2/beers';
+
+  const getBeersButton = document.querySelector('#getBeersButton');
+
+  getBeersButton.addEventListener('click', function(){
+    makeRequest(url,requestComplete)
+  });
+}
+
+const makeRequest = function(url, callback){
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.send();
+
+  request.addEventListener('load', callback);
+}
+
+const requestComplete = function(){
+  if(this.status!== 200) return;
+  const jsonString = this.responseText;
+  localStorage.setItem('beersList', jsonString);
+  const beersList = JSON.parse(jsonString);
+  console.log("beersList", beersList);
+
+  populateList(beersList);
+
+}
+
+const populateList = function(beers) {
+  const mainUl = document.querySelector('#mainUl');
+
+  beers.forEach(function(beer, index){
+    const li = document.createElement('li');
+    li.innerText = beer.name;
+    li.value = index;
+    const img = document.createElement('img');
+    img.style.width = '30px';
+    img.src = beer.image_url;
+    mainUl.appendChild(li);
+    mainUl.appendChild(img);
+
+  })
+}
+
+document.addEventListener('DOMContentLoaded', app);
